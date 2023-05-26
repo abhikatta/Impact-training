723b2a4f3001dad08e4e4134c1fe6e225b51743e
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-26 16:30:12 +0530

    some-more-functions

--- python/DLinkedList.py
+++ python/DLinkedList.py
@@ -4,6 +4,11 @@ class Node:
         self.prev = None
         self.next = None
 
+# TODO:
+# 1. Insert after value
+# 2. insert at index
+# 3. Delete at index
+
 
 class DLL:
     def __init__(self) -> None:
@@ -36,6 +41,48 @@ class DLL:
         self.head = node
         node.next = current
 
+    # def insert_after_value(self,new_value:int,value:int)->None:
+    #     '''
+    #     Insert given value(new_value) after given data(value).
+    #     '''
+    #     current=self.head
+    #     count=0
+    #     while current is not None:
+    #         if
+
+    def delete_at_end(self) -> None:
+        current = self.head
+        prev = None
+        while current.next is not None:
+            prev = current
+            current = current.next
+        prev.next = None
+
+    def delete_at_beginning(self) -> None:
+        '''
+        Delete at beginning from Doubly Linked List.
+        '''
+        current = self.head
+        self.head = current.next
+
+    def delete_value(self, value: int) -> None:
+        '''
+        Delete value from Doubly Linked List.
+        '''
+        current = self.head
+        prev = None
+        if value == self.head.data:
+            self.head = current.next
+        while current is not None:
+            if current.data == value:
+                if prev is None:
+                    prev = current
+                prev.next = current.next
+                current.next.prev = prev
+                return
+            prev = current
+            current = current.next
+
     def traverse(self) -> None:
         '''
         Print out all the nodes in the Doubly Linked List.
@@ -52,6 +99,17 @@ dll = DLL()
 dll.insert_at_end(100)
 dll.insert_at_end(200)
 dll.insert_at_end(300)
+dll.insert_at_end(400)
+dll.insert_at_end(500)
 dll.traverse()
-dll.insert_at_beginning(400)
+# dll.insert_at_beginning(400)
+# dll.traverse()
+print()
+dll.delete_at_end()
+dll.traverse()
+print()
+dll.delete_at_beginning()
+dll.traverse()
+print()
+dll.delete_value(200)
 dll.traverse()
