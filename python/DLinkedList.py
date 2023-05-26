82e92b3da822dae662bc2f61e2b274a18750f177
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-26 12:14:30 +0530

    insertion-begin,end

--- python/DLinkedList.py
+++ python/DLinkedList.py
@@ -17,18 +17,24 @@ class DLL:
         node = Node(data=data)
         if self.head is None:
             self.head = node
+
         else:
             current = self.head
-            prev = current
+
             while current.next is not None:
-                prev = current.prev
+
                 current = current.next
-            prev = current.prev
             current.next = node
             node.prev = current
 
     def insert_at_beginning(self, value: int) -> None:
+        '''
+        Insert an element at the beginning of the Doubly Linked List.
+        '''
         current = self.head
+        node = Node(value)
+        self.head = node
+        node.next = current
 
     def traverse(self) -> None:
         '''
@@ -46,4 +52,6 @@ dll = DLL()
 dll.insert_at_end(100)
 dll.insert_at_end(200)
 dll.insert_at_end(300)
+dll.traverse()
+dll.insert_at_beginning(400)
 dll.traverse()
