c85a638ec9e3b5d514a49d405f94714f6016b575
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-27 21:25:40 +0530

    BST-bug-fix

--- python/BST.py
+++ python/BST.py
@@ -22,15 +22,38 @@ class BST:
     def traverse(self, root):
         if root is not None:
             self.traverse(root=root.prev)
-            print(root.data)
+            print(root.data, end=', ')
             self.traverse(root=root.next)
+
+    def search(self, value, root):
+        count = 0
+        while root is not None:
+            count += 1
+            if value < root.data:
+                return self.search(root=root.prev, value=value)
+            elif value > root.data:
+                return self.search(root=root.next, value=value)
+            elif value == root.data:
+                return True
+        return
+    # def
 
 
 bstree = BST()
-bstree.root = bstree.insert(value=1, root=None)
-bstree.insert(value=10, root=bstree.root)
-bstree.insert(value=11, root=bstree.root)
-bstree.insert(value=12, root=bstree.root)
+
+# Tree should be made as:
+# 1. A root should be created, which can be done in 2 ways:
+# -  a. bstree.root = Node(14)
+# -  b. bstree.root = bstree.insert(value=14, root=None)
+# 2. Pass the nodes as bstree.insert(value=some_value, root=bstree.root)
+
+
+bstree.root = Node(14)
+bstree.insert(value=15, root=bstree.root)
 bstree.insert(value=13, root=bstree.root)
-bstree.insert(value=14, root=bstree.root)
+bstree.insert(value=12, root=bstree.root)
+bstree.insert(value=11, root=bstree.root)
+bstree.insert(value=10, root=bstree.root)
 bstree.traverse(root=bstree.root)
+print()
+print(bstree.search(root=bstree.root, value=13))
