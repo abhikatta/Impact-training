766d0e43f403f09c837a93c1e46b66e2df015df8
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-28 22:44:19 +0530

    'delete-case1'

--- git_automate.py
+++ git_automate.py
@@ -1,6 +1,9 @@
 import os
+import time
 commit_message = str(input("Commit message: "))
 while True:
     os.system('git add .')
     os.system('git commit -m'+commit_message)
     os.system('git push')
+    print("\nTrying again in 10 seconds.\n")
+    time.sleep(secs=10)
