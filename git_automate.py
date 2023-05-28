1d7e28480733d8e16e030e558cb565ed459e089f
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-28 22:42:08 +0530

    'delete-case1'

--- git_automate.py
+++ git_automate.py
@@ -1,5 +1,11 @@
 import os
-commit_message = str(input("Commit message: "))
-os.system('git add .')
-os.system('git commit -m'+commit_message)
-os.system('git push')
+a = True
+while a:
+    try:
+        commit_message = str(input("Commit message: "))
+        os.system('git add .')
+        os.system('git commit -m'+commit_message)
+        os.system('git push')
+        a = False
+    except:
+        continue
