af2a5e8ad65205a9f026de464c19edefab1400fd
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-29 16:35:04 +0530

    simpler

--- gitter.py
+++ gitter.py
@@ -1,19 +1,7 @@
 # TODO:
 # 1. Fix this.
 import os
-import time
 commit_message = str(input("Commit message: "))
-a = False
-while not a:
-    if not a:
-
-        os.system('git add .')
-        os.system('git commit -m'+commit_message)
-        if not os.system('git push'):
-            continue
-        elif os.system('git push'):
-            break
-
-    else:
-        print("\nTrying again in 10 seconds.\n")
-        time.sleep(10)
+os.system('git add .')
+os.system('git commit -m'+commit_message)
+os.system('git push')
