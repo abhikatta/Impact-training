fa6ed27ee0992287cc412d9fcd714fdd95a1ae53
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-05-29 08:59:02 +0530

    .

--- gitter.py
+++ gitter.py
@@ -1,9 +1,14 @@
 import os
 import time
 commit_message = str(input("Commit message: "))
-while True:
-    os.system('git add .')
-    os.system('git commit -m'+commit_message)
-    os.system('git push')
-    print("\nTrying again in 10 seconds.\n")
-    time.sleep(10)
+a = False
+while not a:
+    if not a:
+
+        os.system('git add .')
+        os.system('git commit -m'+commit_message)
+        os.system('git push')
+        a = True
+    else:
+        print("\nTrying again in 10 seconds.\n")
+        time.sleep(10)
